fc53f94fc950e54aaf5efb6228d9ac5ebecd2048
incheol789 <48637681+incheol789@users.noreply.github.com>
2023-02-10 21:53:00 +0900

    SpringBoot CRUD 게시판/댓글 구현

---
+++ firstproject 2/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.*;
+@Entity // DB가 해당 객체를 인식 가능! (해당 클래스로 테이블을 만든다!)
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 전략
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
+}
